21f51d1c9f7cb2ac7f0cbaa37e26a06c7d396452
Niranjan Dukhande <progsolos@gmail.com>
2025-01-31 07:21:27 +0530

    comparisons

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(undefined > 0); //false
+console.log(undefined == 0); //false
+console.log(undefined >= 0); //false
+
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+// equality check == and comparisons > < >= <= work differently
+// comparision convert null to a number, treating it as 0
+// that's why null >= 0 is true and null > 0 is false.
+
+
+// Strick check ===
+
+console.log("2" === 2); //false
+console.log("2" == 2); // true
