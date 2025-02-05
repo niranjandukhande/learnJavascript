bf518fa1e06c37333171ad8a56f238283651b887
Niranjan Dukhande <progsolos@gmail.com>
2025-02-05 12:34:03 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+// Arrays
+
+// resizable, mix of diff types allowed
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[3])
+
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+//to add at start of the array 
+// myArr.unshift(0)
+// myArr.shift()
+
+//console.log(myArr2.includes(9)) // false 
+
+const newArr = myArr.join()
+
+console.log(myArr);
+// the type of newArr is string ;
+// console.log(newArr)
+
+// slice, splice
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1)
+  
+ console.log("B ",myArr)
+
+
+
