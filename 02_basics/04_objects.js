c106c15f617bac6649f5b38203cbef042a2bae4a
Niranjan Dukhande <progsolos@gmail.com>
2025-02-10 22:01:50 +0530

    objects pt2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,46 @@
+// const tinderUser = new Object() //singleton
+// const tinderUser1 = {} // non singleton
+
+// console.log(tinderUser)
+// console.log(tinderUser1)
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullname: {
+    userfullname: {
+      firstname: "prog",
+      lastname: "solos",
+    },
+  },
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = { ...obj1, ...obj2 }
+// console.log(obj3)
+
+const users = [{}, {}]
+
+users[1].email
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser)) // to get all keys
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser)) // key-value
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn")) // to check if a property exists or not
